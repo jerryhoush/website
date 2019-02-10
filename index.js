64b4097b49c19785dba8669659194651a6760b2f
Jerry Housh <jerryhoush@Jerrys-MacBook-Pro.local>
2019-02-10 14:05:17 -0600

    MAINT: updates to index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 app.set('port', (process.env.PORT || 5000))
 app.use('/img', express.static(__dirname + '/img'))
 
